90a6b7280582e426972473ab6ca3242722268542
Nippez <nsederho24@students.oamk.fi>
2026-03-28 12:54:22 +0200

    button nav test

--- SportAI/src/screens/Home.tsx
+++ SportAI/src/screens/Home.tsx
@@ -2,7 +2,21 @@ import React, { useState } from "react";
 import { View, Text, Modal, Pressable, StyleSheet } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Calendar } from "react-native-calendars"; //periaatteessa big-calendar käy myös, tai käyttämällä gridejä.
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
 
+type RootTabParamList = {
+  Test1: undefined;
+  Home: undefined;
+};
+//navigointi eri sivulle ja automaattinen treenin aloittaminen
+//test1 on väliaikaisesti käytössä
+type HomeScreenNavigationProp = BottomTabNavigationProp<
+  RootTabParamList,
+  "Home"
+>;
+type Props = {
+  navigation: HomeScreenNavigationProp;
+};
 type ActivityMap = {
     [key: string]: string;
 };
@@ -10,7 +24,7 @@ const activities: ActivityMap = {
     "2026-03-17": "5km, jep", //pvm + treeni. Myöhemmin vaikka tiedon siirtäminen tietokannasta tähän.
 };
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }: Props) {
     const [selectedDate, setSelectedDate] = useState<string | null>(null);
 
     const getActivityText = () => {
@@ -28,6 +42,12 @@ export default function HomeScreen() {
         markedDates={{
           "2026-03-17": { marked: true }, //Päivämäärä on muotoa yyyy/mm/dd ja marked näyttää kalenterissa merkin päivämäärän kohdalla.
         }}/>
+      
+      <Pressable
+        style={styles.Button}
+         onPress={() => navigation.navigate("Test1")}>
+        <Text style={styles.ButtonText}>Start</Text>
+      </Pressable>
 
       <Modal visible={!!selectedDate}>
         <View style={styles.modalOverlay}>
@@ -76,5 +96,14 @@ const styles = StyleSheet.create({
   closeButton: {
     marginTop: 20,
     color: "red",
+  },
+  Button: {
+    marginTop: 20,
+    padding: 12,
+    alignItems: "center",
+  },
+  ButtonText: {
+    color: "green",
+    fontSize: 16,
   },
 });
