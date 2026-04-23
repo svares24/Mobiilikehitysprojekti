a311071e54f312dad9c3c9a5cbf593b88349ccdb
Markus Rousu <rousu.markus@gmail.com>
2026-04-23 15:20:06 +0300

    various time groups

---
+++ SportAI/src/types/compoundRoute.ts
@@ -0,0 +1,5 @@
+export interface Compound {
+  duration: number;
+  distance: number;
+  period: string;
+}
--- SportAI/src/types/index.ts
+++ SportAI/src/types/index.ts
@@ -2,3 +2,5 @@ export * from './coords';
 export * from './point';
 export * from './route';
 export * from './userData';
+export * from './compoundRoute';
+export * from './period';
